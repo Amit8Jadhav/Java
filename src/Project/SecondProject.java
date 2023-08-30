749f571c916f0b9b766c2a615d6ddca0cddb9f6a
Amit8Jadhav <amitjadhav9796@gmail.com>
2023-08-30 12:35:11 +0530

    added second project file and one modified file

---
+++ src/Project/SecondProject.java
@@ -0,0 +1,16 @@
+package Project;
+
+public class SecondProject {
+
+	public static void main(String[] args) {
+		
+	int a[] = new int[] {1,2,3,4,5};
+		
+		for(int i= a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+		}
+		
+
+	}
+
+}
