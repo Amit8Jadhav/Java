9ff511658c473eade8b6a2472001fc3879b4a696
Amit8Jadhav <amitjadhav9796@gmail.com>
2023-08-30 12:13:06 +0530

    first project

---
+++ src/Project/FirstProject.java
@@ -0,0 +1,28 @@
+package Project;
+
+
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+	
+		int a[] = new int[] {1,2,33,4,5};
+		int max =0;
+		
+		for(int i=0;i<a.length;i++) {
+			
+			if(a[i]>max) {
+				max=a[i];
+			}
+			
+		}
+		
+		System.out.println(max);
+      
+       
+        
+         
+		
+	}
+
+}
